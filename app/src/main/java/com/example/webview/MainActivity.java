9d60560e129c98fbd10d622e84620f4cb24bc98f
Moses9-k <kirathimoses@gmail.com>
2020-08-15 09:23:40 +0300

    first learnt

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView =(WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+      // webView.loadUrl("https://www.google.com");
+       webView.loadData("<html><body><p> hello peter </p></body></html>","text/html","UTF-8");
+    }
+}
